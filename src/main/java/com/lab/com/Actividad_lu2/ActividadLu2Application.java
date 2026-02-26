c9920240f9caa09b558d9665e27947881cbf5b48
LUIS <ing.luisgonzalez1994@gmail.com>
2026-02-25 20:30:00 -0500

    commit inicia

---
+++ src/main/java/com/lab/com/Actividad_lu2/ActividadLu2Application.java
@@ -0,0 +1,28 @@
+package com.lab.com.Actividad_lu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ActividadLu2Application {
+    public class ActividadLabApplication {
+        @GetMapping("/saludo")
+        public String saludar() {
+            return "Bienvenido a la actividad";
+        }
+
+        @GetMapping("/saludos")
+        public String saludarnos() {
+            return "llego la hora de empezar a programar cd/ci";
+        }
+    }
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(ActividadLu2Application.class, args);
+        }
+
+    }
---
+++ src/test/java/com/lab/com/Actividad_lu2/ActividadLu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.com.Actividad_lu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadLu2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
