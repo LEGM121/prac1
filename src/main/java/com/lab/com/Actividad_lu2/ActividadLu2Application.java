e14406aa6e290931e76a45bfeae970a234e6621f
LUIS <ing.luisgonzalez1994@gmail.com>
2026-02-25 20:52:02 -0500

    commit inicia

--- src/main/java/com/lab/com/Actividad_lu2/ActividadLu2Application.java
+++ src/main/java/com/lab/com/Actividad_lu2/ActividadLu2Application.java
@@ -14,8 +14,8 @@ public class ActividadLu2Application {
             return "Bienvenido a la actividad";
         }
 
-        @GetMapping("/saludos")
-        public String saludarnos() {
+        @GetMapping("/saludar")
+        public String contexto() {
             return " la hora de empezar a programar cd/ci";
         }
     }
